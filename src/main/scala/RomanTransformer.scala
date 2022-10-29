aeff4d6a8053688bbfbf35539fadf9901ed2bb68
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2022-10-29 07:34:20 +0200

    Simple Refactor

--- src/main/scala/RomanTransformer.scala
+++ src/main/scala/RomanTransformer.scala
@@ -2,22 +2,24 @@
 class RomanTransformer extends BasicMapping {
   def roman(number: Int): String = {
       basic(number)
-      .map(roman => roman.asString())
+      .map(_.asString())
       .getOrElse(???)
   }
 }
 
 trait BasicMapping {
   private val BasicTypes = Seq(
-    Tuple2(1, Roman.Basic('I')),
-    Tuple2(5, Roman.Basic('V')),
-    Tuple2(10, Roman.Basic('X')),
-    Tuple2(50, Roman.Basic('L')),
-    Tuple2(100, Roman.Basic('C')),
-    Tuple2(500, Roman.Basic('D')),
-    Tuple2(1000, Roman.Basic('M'))
+    (1, Roman.Basic('I')),
+    (5, Roman.Basic('V')),
+    (10, Roman.Basic('X')),
+    (50, Roman.Basic('L')),
+    (100, Roman.Basic('C')),
+    (500, Roman.Basic('D')),
+    (1000, Roman.Basic('M'))
   )
 
   def basic(number: Int): Option[Roman] = 
-    BasicTypes.find(_._1 == number).map(pair => pair._2)
+    BasicTypes
+      .toMap
+      .get(number)
 }
