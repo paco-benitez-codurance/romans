2469fd6ebb60f5698cd0346ebb9451e05d6271b3
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2022-10-29 06:31:19 +0200

    Basic Number

--- src/main/scala/DecimalNumber.scala
+++ src/main/scala/DecimalNumber.scala
@@ -1,15 +1,26 @@
-class DecimalNumber(number: Int) 
+class DecimalNumber(number: Int)
 
 object DecimalNumber {
-    def apply(number: Int): DecimalNumber = {
-        if(number == 1) return BasicDecimalNumber(1, RomanNumber('I'))
-        if(number == 5) return BasicDecimalNumber(1, RomanNumber('V'))
-        ???
-    }
+  def apply(number: Int): DecimalNumber = {
+    val isBasic = BasicDecimalNumber.basic.find(_.number == number)
+    if (isBasic.isDefined) return isBasic.get
+    ???
+  }
 }
 
-
-class BasicDecimalNumber(number: Int, romanNumber: RomanNumber) extends DecimalNumber(number) {
-    def toRoman(): RomanNumber = romanNumber
+class BasicDecimalNumber(val number: Int, val romanNumber: RomanNumber)
+    extends DecimalNumber(number) {
+  def toRoman(): RomanNumber = romanNumber
 }
 
+object BasicDecimalNumber {
+  val basic = Seq(
+    BasicDecimalNumber(1, RomanNumber('I')),
+    BasicDecimalNumber(5, RomanNumber('V')),
+    BasicDecimalNumber(10, RomanNumber('X')),
+    BasicDecimalNumber(50, RomanNumber('L')),
+    BasicDecimalNumber(100, RomanNumber('C')),
+    BasicDecimalNumber(500, RomanNumber('D')),
+    BasicDecimalNumber(1000, RomanNumber('M'))
+  )
+}
--- src/test/scala/RomansTransformerShould.scala
+++ src/test/scala/RomansTransformerShould.scala
@@ -5,7 +5,7 @@ import org.scalatest._
 import org.scalatest.prop.TableDrivenPropertyChecks
 import matchers._
 
-class RomansShould
+class RomansTransformerShould
     extends AnyFreeSpec
     with TableDrivenPropertyChecks
     with should.Matchers {
@@ -14,7 +14,12 @@ class RomansShould
     Table(
       ("decimal", "roman"),
       (1, 'I'),
-      (5, 'V')
+      (5, 'V'),
+      (10, 'X'),
+      (50, 'L'),
+      (100, 'C'),
+      (500, 'D'),
+      (1000, 'M')
     )
 
   forAll(basicNumber) { (decimal, roman) => {
