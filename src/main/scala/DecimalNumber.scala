ffd41ac75836055bbc43dc5f4573a23dc1a3d280
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2022-10-29 06:46:15 +0200

    Collection first actor

--- src/main/scala/DecimalNumber.scala
+++ src/main/scala/DecimalNumber.scala
@@ -1,11 +1,7 @@
 class DecimalNumber(number: Int)
 
-object DecimalNumber {
-  def apply(number: Int): DecimalNumber = {
-    val isBasic = BasicDecimalNumber.basic.find(_.number == number)
-    if (isBasic.isDefined) return isBasic.get
-    ???
-  }
+object DecimalNumber extends BasicDecimalNumberCollections {
+  def apply(number: Int): DecimalNumber = basic(number).getOrElse(???)
 }
 
 class BasicDecimalNumber(val number: Int, val romanNumber: RomanNumber)
@@ -13,8 +9,8 @@ class BasicDecimalNumber(val number: Int, val romanNumber: RomanNumber)
   def toRoman(): RomanNumber = romanNumber
 }
 
-object BasicDecimalNumber {
-  val basic = Seq(
+trait BasicDecimalNumberCollections {
+  private val BasicTypes = Seq(
     BasicDecimalNumber(1, RomanNumber('I')),
     BasicDecimalNumber(5, RomanNumber('V')),
     BasicDecimalNumber(10, RomanNumber('X')),
@@ -23,4 +19,7 @@ object BasicDecimalNumber {
     BasicDecimalNumber(500, RomanNumber('D')),
     BasicDecimalNumber(1000, RomanNumber('M'))
   )
+
+  def basic(number: Int): Option[BasicDecimalNumber] = BasicTypes.find(_.number == number)
+
 }
