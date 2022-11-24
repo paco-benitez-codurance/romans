686d2546ab28ade95c4087f53ab52221b3f5f783
Paco Benitez <paco.benitez@sfd.gov.sa>
2022-11-24 09:02:07 +0100

    Add One to Left

---
+++ src/main/scala/AddToLeft.scala
@@ -0,0 +1,18 @@
+import cats._
+import cats.data._
+import cats.implicits._
+
+class AddOneToLeft() extends MapRoman {
+  def basic(number: Int): Option[(Int, Basic)] = {
+    BasicTypes.reverse.find(_._1 == number)
+  }
+  override def roman(number: Int): Option[Roman] = {
+    val solutions = for {
+      basic <- BasicTypes
+      right <- BasicTypes if(basic._1 > right._1 )
+      if(basic._1 - right._1 == number)
+    } yield Roman(right._2) |+|  Roman(basic._2)
+
+    solutions.headOption
+  }
+}
---
+++ src/test/scala/AddOneToLeftShould.scala
@@ -0,0 +1,21 @@
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest._
+import org.scalatest.prop.TableDrivenPropertyChecks
+import matchers._
+
+class AddOneToLeftShould
+    extends AnyFreeSpec
+    with should.Matchers {
+
+  val mappingOne = AddOneToLeft()
+
+  def check(mapping: MapRoman, decimal: Int, expected: String) = {
+      s"$decimal should be $expected" in {
+        mapping.roman(decimal).map(_.asString()).get shouldBe expected
+      }
+  }
+
+  check(mappingOne, 4, "IV")
+  check(mappingOne, 9, "IX")
+
+}
